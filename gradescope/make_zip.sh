6062244d39ced0c7c3ee1f0f04f1286f8b2f84e5
Emmanuel Orozco <indi3.rok@gmail.com>
2023-09-27 21:39:53 +0200

    Added gradescope stuff

---
+++ gradescope/make_zip.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Loop through all subdirectories to find test files
+test_cases=$(find .. -type f -name "*.test.js" -maxdepth 2)
+for test in $test_cases; do
+  # Get the parent directory name
+  parent_dir=$(basename "$(dirname "$test")")
+  mkdir -p ${parent_dir}
+  cp "${test}" "${test#*/}"
+done
+
+find . -type f -name '*.test.js' -exec zip gradescope.zip setup.sh run_autograder package.json jest.config.js {} +
+
+# Remove test files from gradescope after zip
+rm ./*.test.js
